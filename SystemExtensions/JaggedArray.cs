346110a786ecc7d16cfb6046ad53167426faf56b
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-05-29 19:00:25 -0400

    Added support for initializing 1D and 2D jagged arrays via 0- and 1-parameter func delegates.

--- SystemExtensions/JaggedArray.cs
+++ SystemExtensions/JaggedArray.cs
@@ -4,20 +4,27 @@ namespace SystemExtensions
 {
     public static class JaggedArray
     {
+        //===============================================================================
+        //                                1D ARRAYS
+        //===============================================================================
+
+
         /// <summary>
-        /// Returns a 1D array of size X.
+        /// Returns a new 1D array of length x. Each element is initialized to T's default value.
         /// </summary>
-        /// <typeparam name="T">The type of the elements of the array</typeparam>
-        /// <param name="x">The size of the array</param>
+        /// <typeparam name="T">The array type.</typeparam>
+        /// <param name="x">The length of the array.</param>
         public static T[] Create<T>(int x)
         {
             return new T[x];
         }
 
         /// <summary>
-        /// Returns an array of size X. Every element is initialized to either VAL, if value type,
-        /// or with new instances from the type's default constructor, if reference type.
+        /// Returns a new 1D array of length x. Every element is initialized to either val, if value type, or with new instances from the type's default constructor, if reference type.
         /// </summary>
+        /// <typeparam name="T">The array type.</typeparam>
+        /// <param name="x">The length of the array.</param>
+        /// <param name="val">The initialization value.</param>
         public static T[] Create<T>(int x, T val)
         {
             T[] output = new T[x];
@@ -30,14 +37,59 @@ namespace SystemExtensions
             return output;
         }
 
-
-
-
-
+        /// <summary>
+        /// Returns a new array of length x. Every element is initialized to the return value of the given func delegate.
+        /// </summary>
+        /// <typeparam name="T">The array type.</typeparam>
+        /// <param name="x">The size of the array.</param>
+        /// <param name="func">A delegate to initialize each element.</param>
+        /// <returns></returns>
+        public static T[] Create<T>(int x, Func<T> func)
+        {
+            T[] output = new T[x];
+            for (int i = 0; i < x; i++)
+                output[i] = func.Invoke();
+            return output;
+        }
 
         /// <summary>
-        /// Returns a 2D array of size X, Y.
+        /// Returns a new array of length x. Every element is initialized to the return value of the given func delegate, which is given funcArgs as input.
         /// </summary>
+        /// <typeparam name="T">The array type.</typeparam>
+        /// <typeparam name="TFuncArgs">The type of the input parameter for the func delegate.</typeparam>
+        /// <param name="x">The length of the array.</param>
+        /// <param name="funcArgs">The input given to the func delegate.</param>
+        /// <param name="func">A delegate to initialize each element.</param>
+        /// <returns></returns>
+        public static T[] Create<T, TFuncArgs>(int x, TFuncArgs funcArgs, Func<TFuncArgs, T> func)
+        {
+            T[] output = new T[x];
+            for (int i = 0; i < x; i++)
+                output[i] = func.Invoke(funcArgs);
+            return output;
+        }
+
+
+
+
+
+
+
+
+
+
+
+        //===============================================================================
+        //                                2D ARRAYS
+        //===============================================================================
+        
+        /// <summary>
+        /// Returns a new 2D array of lengths x, y. Each element is initialized to T's default value.
+        /// </summary>
+        /// <typeparam name="T">The array type.</typeparam>
+        /// <param name="x">The length of the first array.</param>
+        /// <param name="y">The length of each secondary array.</param>
+        /// <returns></returns>
         public static T[][] Create<T>(int x, int y)
         {
             T[][] output = new T[x][];
@@ -46,9 +98,13 @@ namespace SystemExtensions
             return output;
         }
         /// <summary>
-        /// Returns a 2D array of size X, Y.  Every element is initialized to either VAL, if value type,
-        /// or with new instances from the type's default constructor, if reference type.
+        /// Returns a new 2D array of lengths x, y.  Every element is initialized to either val, if value type, or with new instances from the type's default constructor, if reference type.
         /// </summary>
+        /// <typeparam name="T">The array type.</typeparam>
+        /// <param name="x">The length of the first array.</param>
+        /// <param name="y">The length of each secondary array.</param>
+        /// <param name="val">The initialization value.</param>
+        /// <returns></returns>
         public static T[][] Create<T>(int x, int y, T val)
         {
             T[][] output = new T[x][];
@@ -70,6 +126,59 @@ namespace SystemExtensions
             return output;
         }
 
+        /// <summary>
+        /// Returns a 2D array of lengths x, y. Every element is initialized to the return value of the given func delegate.
+        /// </summary>
+        /// <typeparam name="T">The array type.</typeparam>
+        /// <param name="x">The length of the first array.</param>
+        /// <param name="y">The length of each secondary array.</param>
+        /// <param name="func">A delegate to initialize each element.</param>
+        /// <returns></returns>
+        public static T[][] Create<T>(int x, int y, Func<T> func)
+        {
+            T[][] output = new T[x][];
+            for (int i = 0; i < x; i++)
+            {
+                output[i] = new T[y];
+                for (int j = 0; j < y; j++)
+                    output[i][j] = func.Invoke();
+            }
+            return output;
+        }
+
+        /// <summary>
+        /// Returns a new 2D array of lengths x, y. Every element is initialized to the return value of the given func delegate, which is given funcArgs as input.
+        /// </summary>
+        /// <typeparam name="T">The array type.</typeparam>
+        /// <typeparam name="TFuncArgs">The type of the input parameter for the func delegate.</typeparam>
+        /// <param name="x">The length of the first array.</param>
+        /// <param name="y">The length of each seconday array.</param>
+        /// <param name="funcArgs">The input given to the func delegate.</param>
+        /// <param name="func">A delegate to initialize each element.</param>
+        /// <returns></returns>
+        public static T[][] Create<T, TFuncArgs>(int x, int y, TFuncArgs funcArgs, Func<TFuncArgs, T> func)
+        {
+            T[][] output = new T[x][];
+            for (int i = 0; i < x; i++)
+            {
+                output[i] = new T[y];
+                for (int j = 0; j < y; j++)
+                    output[i][j] = func.Invoke(funcArgs);
+            }
+            return output;
+        }
+
+
+
+
+
+
+
+
+
+        //===============================================================================
+        //                                3D ARRAYS
+        //===============================================================================
 
         /// <summary>
         /// Returns a 3D array of size X, Y, Z

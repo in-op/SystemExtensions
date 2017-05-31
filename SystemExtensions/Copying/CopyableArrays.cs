a6f139e60fdb61b22217b7d53fe364d7b9ca229b
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-05-31 06:19:52 -0400

    Added support for parallel deep copies of arrays. Updated version number

--- SystemExtensions/Copying/CopyableArrays.cs
+++ SystemExtensions/Copying/CopyableArrays.cs
@@ -42,10 +42,10 @@ namespace SystemExtensions.Copying
 
         /// <summary>
         /// Returns a deep copy of the calling array utilizing multithreading.
-        /// If the array type <typeparamref name="T"/> is a reference type it
+        /// If the array type T is a reference type it
         /// must implement a parameterless instance
         /// method DeepCopy(), which returns a deep
-        /// copy of the instance. If <typeparamref name="T"/> is a value type,
+        /// copy of the instance. If T is a value type,
         /// it must be immutable.
         /// </summary>
         /// <typeparam name="T">The array type.</typeparam>
@@ -125,6 +125,49 @@ namespace SystemExtensions.Copying
         }
 
 
+        /// <summary>
+        /// Returns a deep copy of the calling array.
+        /// If the array type T is a reference type it
+        /// must implement a parameterless instance
+        /// method DeepCopy(), which returns a deep
+        /// copy of the instance. If T is a value type,
+        /// it must be immutable.
+        /// </summary>
+        public static T[][] ParallelDeepCopy<T>(this T[][] array)
+        {
+            int x = array.Length;
+            T[][] copy = new T[x][];
+            if (typeof(T).IsValueType)
+                System.Threading.Tasks.Parallel.For(0, x, (i) =>
+                {
+                    int y = array[i].Length;
+                    copy[i] = new T[y];
+                    for (int j = 0; j < y; j++)
+                        copy[i][j] = array[i][j];
+                });
+            else
+                System.Threading.Tasks.Parallel.For(0, x, (i) =>
+                {
+                    int y = array[i].Length;
+                    copy[i] = new T[y];
+                    for (int j = 0; j < y; j++)
+                        if (array[i][j] == null) copy[i][j] = default(T);
+                        else
+                            try
+                            {
+                                object item = array[i][j];
+                                var deepCopy = item.GetType().GetMethod("DeepCopy");
+                                copy[i][j] = (T)deepCopy.Invoke(item, new object[0]);
+                            }
+                            catch
+                            {
+                                throw new Exception("The array type " + typeof(T).Name + " must implement a parameterless instance method DeepCopy() which returns a deep copy of the instance.");
+                            }
+                });
+            return copy;
+        }
+
+
 
 
         /// <summary>
@@ -142,10 +185,12 @@ namespace SystemExtensions.Copying
             if (typeof(T).IsValueType)
                 for (int i = 0; i < x; i++)
                 {
-                    int y = copy[i].Length;
+                    int y = array[i].Length;
+                    copy[i] = new T[y][];
                     for (int j = 0; j < y; j++)
                     {
-                        int z = copy[i][j].Length;
+                        int z = array[i][j].Length;
+                        copy[i][j] = new T[z];
                         for (int k = 0; k < z; k++)
                             copy[i][j][k] = array[i][j][k];
                     }
@@ -153,10 +198,12 @@ namespace SystemExtensions.Copying
             else
                 for (int i = 0; i < x; i++)
                 {
-                    int y = copy[i].Length;
+                    int y = array[i].Length;
+                    copy[i] = new T[y][];
                     for (int j = 0; j < y; j++)
                     {
-                        int z = copy[i][j].Length;
+                        int z = array[i][j].Length;
+                        copy[i][j] = new T[z];
                         for (int k = 0; k < z; k++)
                             if (array[i][j][k] == null) copy[i][j][k] = default(T);
                             else
@@ -172,6 +219,59 @@ namespace SystemExtensions.Copying
                                 }
                     }
                 }
+            return copy;
+        }
+
+
+        /// <summary>
+        /// Returns a deep copy of the calling array.
+        /// If the array type T is a reference type it
+        /// must implement a parameterless instance
+        /// method DeepCopy(), which returns a deep
+        /// copy of the instance. If T is a value type,
+        /// it must be immutable.
+        /// </summary>
+        public static T[][][] ParallelDeepCopy<T>(this T[][][] array)
+        {
+            int x = array.Length;
+            T[][][] copy = new T[x][][];
+            if (typeof(T).IsValueType)
+                System.Threading.Tasks.Parallel.For(0, x, (i) =>
+                {
+                    int y = array[i].Length;
+                    copy[i] = new T[y][];
+                    for (int j = 0; j < y; j++)
+                    {
+                        int z = array[i][j].Length;
+                        copy[i][j] = new T[z];
+                        for (int k = 0; k < z; k++)
+                            copy[i][j][k] = array[i][j][k];
+                    }
+                });
+            else
+                System.Threading.Tasks.Parallel.For(0, x, (i) =>
+                {
+                    int y = array[i].Length;
+                    copy[i] = new T[y][];
+                    for (int j = 0; j < y; j++)
+                    {
+                        int z = array[i][j].Length;
+                        copy[i][j] = new T[z];
+                        for (int k = 0; k < z; k++)
+                            if (array[i][j][k] == null) copy[i][j][k] = default(T);
+                            else
+                                try
+                                {
+                                    object item = array[i][j][k];
+                                    var deepCopy = item.GetType().GetMethod("DeepCopy");
+                                    copy[i][j][k] = (T)deepCopy.Invoke(item, new object[0]);
+                                }
+                                catch
+                                {
+                                    throw new Exception("The array type " + typeof(T).Name + " must implement a parameterless instance method DeepCopy() which returns a deep copy of the instance.");
+                                }
+                    }
+                });
             return copy;
         }
 
@@ -195,13 +295,16 @@ namespace SystemExtensions.Copying
             if (typeof(T).IsValueType)
                 for (int i = 0; i < x; i++)
                 {
-                    int y = copy[i].Length;
+                    int y = array[i].Length;
+                    copy[i] = new T[y][][];
                     for (int j = 0; j < y; j++)
                     {
-                        int z = copy[i][j].Length;
+                        int z = array[i][j].Length;
+                        copy[i][j] = new T[z][];
                         for (int k = 0; k < z; k++)
                         {
-                            int a = copy[i][j][k].Length;
+                            int a = array[i][j][k].Length;
+                            copy[i][j][k] = new T[a];
                             for (int m = 0; m < a; m++)
                                 copy[i][j][k][m] = array[i][j][k][m];
                         }
@@ -210,13 +313,16 @@ namespace SystemExtensions.Copying
             else
                 for (int i = 0; i < x; i++)
                 {
-                    int y = copy[i].Length;
+                    int y = array[i].Length;
+                    copy[i] = new T[y][][];
                     for (int j = 0; j < y; j++)
                     {
-                        int z = copy[i][j].Length;
+                        int z = array[i][j].Length;
+                        copy[i][j] = new T[z][];
                         for (int k = 0; k < z; k++)
                         {
-                            int a = copy[i][j][k].Length;
+                            int a = array[i][j][k].Length;
+                            copy[i][j][k] = new T[a];
                             for (int m = 0; m < a; m++)
                                 if (array[i][j][k][m] == null) copy[i][j][k][m] = default(T);
                                 else
@@ -237,6 +343,69 @@ namespace SystemExtensions.Copying
         }
 
 
+        /// <summary>
+        /// Returns a deep copy of the calling array.
+        /// If the array type T is a reference type it
+        /// must implement a parameterless instance
+        /// method DeepCopy(), which returns a deep
+        /// copy of the instance. If T is a value type,
+        /// it must be immutable.
+        /// </summary>
+        public static T[][][][] ParallelDeepCopy<T>(this T[][][][] array)
+        {
+            int x = array.Length;
+            T[][][][] copy = new T[x][][][];
+            if (typeof(T).IsValueType)
+                System.Threading.Tasks.Parallel.For(0, x, (i) =>
+                {
+                    int y = array[i].Length;
+                    copy[i] = new T[y][][];
+                    for (int j = 0; j < y; j++)
+                    {
+                        int z = array[i][j].Length;
+                        copy[i][j] = new T[z][];
+                        for (int k = 0; k < z; k++)
+                        {
+                            int a = array[i][j][k].Length;
+                            copy[i][j][k] = new T[a];
+                            for (int m = 0; m < a; m++)
+                                copy[i][j][k][m] = array[i][j][k][m];
+                        }
+                    }
+                });
+            else
+                System.Threading.Tasks.Parallel.For(0, x, (i) =>
+                {
+                    int y = array[i].Length;
+                    copy[i] = new T[y][][];
+                    for (int j = 0; j < y; j++)
+                    {
+                        int z = array[i][j].Length;
+                        copy[i][j] = new T[z][];
+                        for (int k = 0; k < z; k++)
+                        {
+                            int a = array[i][j][k].Length;
+                            copy[i][j][k] = new T[a];
+                            for (int m = 0; m < a; m++)
+                                if (array[i][j][k][m] == null) copy[i][j][k][m] = default(T);
+                                else
+                                    try
+                                    {
+                                        object item = array[i][j][k][m];
+                                        var deepCopy = item.GetType().GetMethod("DeepCopy");
+                                        copy[i][j][k][m] = (T)deepCopy.Invoke(item, new object[0]);
+                                    }
+                                    catch
+                                    {
+                                        throw new Exception("The array type " + typeof(T).Name + " must implement a parameterless instance method DeepCopy() which returns a deep copy of the instance.");
+                                    }
+                        }
+                    }
+                });
+            return copy;
+        }
+
+
 
         /// <summary>
         /// Returns a deep copy of the calling array.
@@ -253,16 +422,20 @@ namespace SystemExtensions.Copying
             if (typeof(T).IsValueType)
                 for (int i = 0; i < x; i++)
                 {
-                    int y = copy[i].Length;
+                    int y = array[i].Length;
+                    copy[i] = new T[y][][][];
                     for (int j = 0; j < y; j++)
                     {
-                        int z = copy[i][j].Length;
+                        int z = array[i][j].Length;
+                        copy[i][j] = new T[z][][];
                         for (int k = 0; k < z; k++)
                         {
-                            int a = copy[i][j][k].Length;
+                            int a = array[i][j][k].Length;
+                            copy[i][j][k] = new T[a][];
                             for (int m = 0; m < a; m++)
                             {
-                                int b = copy[i][j][k][m].Length;
+                                int b = array[i][j][k][m].Length;
+                                copy[i][j][k][m] = new T[b];
                                 for (int n = 0; n < b; n++)
                                     copy[i][j][k][m][n] = array[i][j][k][m][n];
                             }
@@ -272,16 +445,20 @@ namespace SystemExtensions.Copying
             else
                 for (int i = 0; i < x; i++)
                 {
-                    int y = copy[i].Length;
+                    int y = array[i].Length;
+                    copy[i] = new T[y][][][];
                     for (int j = 0; j < y; j++)
                     {
-                        int z = copy[i][j].Length;
+                        int z = array[i][j].Length;
+                        copy[i][j] = new T[z][][];
                         for (int k = 0; k < z; k++)
                         {
-                            int a = copy[i][j][k].Length;
+                            int a = array[i][j][k].Length;
+                            copy[i][j][k] = new T[a][];
                             for (int m = 0; m < a; m++)
                             {
-                                int b = copy[i][j][k][m].Length;
+                                int b = array[i][j][k][m].Length;
+                                copy[i][j][k][m] = new T[b];
                                 for (int n = 0; n < b; n++)
                                     if (array[i][j][k][m][n] == null) copy[i][j][k][m][n] = default(T);
                                     else
@@ -312,7 +489,12 @@ namespace SystemExtensions.Copying
 
 
 
-
+        /// <summary>
+        /// Returns a deep copy of the calling array, creating multiple parts of the output array in parallel.
+        /// </summary>
+        /// <typeparam name="T">The array type.</typeparam>
+        /// <param name="array">The calling array.</param>
+        /// <returns>A deep copy of the array.</returns>
         public static T[][][][][] ParallelDeepCopy<T>(this T[][][][][] array)
         {
             int x = array.Length;
@@ -321,16 +503,20 @@ namespace SystemExtensions.Copying
             if (typeof(T).IsValueType)
                 System.Threading.Tasks.Parallel.For(0, x, (i) =>
                 {
-                    int y = copy[i].Length;
+                    int y = array[i].Length;
+                    copy[i] = new T[y][][][];
                     for (int j = 0; j < y; j++)
                     {
-                        int z = copy[i][j].Length;
+                        int z = array[i][j].Length;
+                        copy[i][j] = new T[z][][];
                         for (int k = 0; k < z; k++)
                         {
-                            int a = copy[i][j][k].Length;
+                            int a = array[i][j][k].Length;
+                            copy[i][j][k] = new T[a][];
                             for (int m = 0; m < a; m++)
                             {
-                                int b = copy[i][j][k][m].Length;
+                                int b = array[i][j][k][m].Length;
+                                copy[i][j][k][m] = new T[b];
                                 for (int n = 0; n < b; n++)
                                     copy[i][j][k][m][n] = array[i][j][k][m][n];
                             }
@@ -341,16 +527,20 @@ namespace SystemExtensions.Copying
             else
                 System.Threading.Tasks.Parallel.For(0, x, (i) =>
                 {
-                    int y = copy[i].Length;
+                    int y = array[i].Length;
+                    copy[i] = new T[y][][][];
                     for (int j = 0; j < y; j++)
                     {
-                        int z = copy[i][j].Length;
+                        int z = array[i][j].Length;
+                        copy[i][j] = new T[z][][];
                         for (int k = 0; k < z; k++)
                         {
-                            int a = copy[i][j][k].Length;
+                            int a = array[i][j][k].Length;
+                            copy[i][j][k] = new T[a][];
                             for (int m = 0; m < a; m++)
                             {
-                                int b = copy[i][j][k][m].Length;
+                                int b = array[i][j][k][m].Length;
+                                copy[i][j][k][m] = new T[b];
                                 for (int n = 0; n < b; n++)
                                     if (array[i][j][k][m][n] == null) copy[i][j][k][m][n] = default(T);
                                     else
--- SystemExtensionsConsoleTests/Program.cs
+++ SystemExtensionsConsoleTests/Program.cs
@@ -10,25 +10,80 @@ namespace CSharpExtrasConsoleTests
     {
         static void Main(string[] args)
         {
-           // Array5DPerformanceTest();
+            //Array5DPerformanceTest();
+            Array1DPerformanceTest();
         }
-        
 
-        private static void Array5DPerformanceTest()
+
+        private static void Array1DPerformanceTest()
         {
             Stopwatch sw = new Stopwatch();
 
-            int[][][][][] ints = JaggedArray.Create(10, 10, 10, 10, 10, 42);
-            int[][][][][] holder = JaggedArray.Create<int>(10, 10, 10, 10, 50);
+            int size = 1000000;
+            long val = 42L;
+
+            long[] ints = JaggedArray.Create(size, val);
+            long[] holder;
+
+            sw.Reset();
+            sw.Start();
+            for (int i = 0; i < 200; i++)
+            {
+                holder = ints.DeepCopy();
+            }
+            sw.Stop();
+
+            sw.Reset();
+            sw.Start();
+            for (int i = 0; i < 200; i++)
+            {
+                holder = ints.ParallelDeepCopy();
+            }
+            sw.Stop();
+
+
+
+
 
             sw.Reset();
             sw.Start();
             holder = ints.DeepCopy();
             sw.Stop();
+            Console.WriteLine("Single-Threaded: " + sw.ElapsedTicks);
 
             sw.Reset();
             sw.Start();
             holder = ints.ParallelDeepCopy();
+            sw.Stop();
+            Console.WriteLine("Multi-Threaded: " + sw.ElapsedTicks);
+
+            Console.ReadLine();
+        }
+
+        private static void Array5DPerformanceTest()
+        {
+            Stopwatch sw = new Stopwatch();
+
+            int size = 12;
+            long val = 42L;
+
+            long[][][][][] ints = JaggedArray.Create(size, size, size, size, size, val);
+            long[][][][][] holder;
+
+            sw.Reset();
+            sw.Start();
+            for (int i = 0; i < 600; i++)
+            {
+                holder = ints.DeepCopy();
+            }
+            sw.Stop();
+
+            sw.Reset();
+            sw.Start();
+            for (int i = 0; i < 600; i++)
+            {
+                holder = ints.ParallelDeepCopy();
+            }
             sw.Stop();
 
 

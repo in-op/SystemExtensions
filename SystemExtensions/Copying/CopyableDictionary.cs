a645193d223990f51ec513deb101dc1f9c51ccf9
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-06-01 04:45:34 -0400

    Fixed copyable dictionary to correctly copy contents based on checking whether value or reference type and safely catching exception if the key or value types do not implement a DeepCopy() method

--- SystemExtensions/Copying/CopyableDictionary.cs
+++ SystemExtensions/Copying/CopyableDictionary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace SystemExtensions.Copying
 {
@@ -19,8 +20,59 @@ namespace SystemExtensions.Copying
         {
             var copy = new Dictionary<TKey, TValue>(dict.Count);
             foreach (KeyValuePair<TKey, TValue> kvp in dict)
-                copy.Add(kvp.Key, kvp.Value);
+                if (typeof(TKey).IsValueType)
+                {
+                    if (typeof(TValue).IsValueType)
+                        copy.Add(kvp.Key, kvp.Value);
+                    else
+                        try
+                        {
+                            object value = kvp.Value;
+                            MethodInfo deepcopy = value.GetType().GetMethod("DeepCopy");
+                            copy.Add(kvp.Key, (TValue)deepcopy.Invoke(value, new object[0]));
+                        }
+                        catch (ArgumentNullException)
+                        {
+                            throw new Exception("The value type " + typeof(TValue).Name + " must implement a parameterless instance method DeepCopy() that returns a deep copy of the instance.");
+                        }
+                }
+                else
+                {
+                    if (typeof(TValue).IsValueType)
+                        try
+                        {
+                            object key = kvp.Key;
+                            MethodInfo deepcopy = key.GetType().GetMethod("DeepCopy");
+                            copy.Add((TKey)deepcopy.Invoke(key, new object[0]), kvp.Value);
+                        }
+                        catch (ArgumentNullException)
+                        {
+                            throw new Exception("The key type " + typeof(TKey).Name + " must implement a parameterless instance method DeepCopy() that returns a deep copy of the instance.");
+                        }
+                    else
+                        try
+                        {
+                            object key = kvp.Key;
+                            object value = kvp.Value;
+                            MethodInfo keyDeepCopy = key.GetType().GetMethod("DeepCopy");
+                            MethodInfo valueDeepCopy = value.GetType().GetMethod("DeepCopy");
+                            copy.Add((TKey)keyDeepCopy.Invoke(key, new object[0]),
+                                (TValue)valueDeepCopy.Invoke(value, new object[0]));
+                        }
+                        catch (ArgumentNullException)
+                        {
+                            throw new Exception("Both the key type " + typeof(TKey).Name + " and the value type " + typeof(TValue).Name + " must implement a parameterless instance method DeepCopy() that returns a deep copy of the instance.");
+                        }
+                }
             return copy;
         }
+
+
+
+
+
+
+
+
     }
 }

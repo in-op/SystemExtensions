d70b3160ab3c1dbd2792067b841096aea78bf864
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-06-17 17:40:45 -0400

    Updated to v0.6, simplified implementation of JaggedArray

--- SystemExtensions/JaggedArray.cs
+++ SystemExtensions/JaggedArray.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 
 namespace SystemExtensions
 {
+    /// <summary>
+    /// A class of static generic methods for initializing jagged arrays
+    /// of multiple dimensions.
+    /// </summary>
     public static class JaggedArray
     {
         //===============================================================================
@@ -21,20 +25,16 @@ namespace SystemExtensions
         }
 
         /// <summary>
-        /// Returns a new 1D array of length x. Each element is initialized to either val, if value type, or with new instances from the type's default constructor, if reference type.
+        /// Returns a new 1D array of length x. Each element is initialized to val.
         /// </summary>
         /// <typeparam name="T">The array type.</typeparam>
         /// <param name="x">The length of the array.</param>
         /// <param name="val">The initialization value.</param>
-        public static T[] Create<T>(int x, T val)
+        public static T[] Create<T>(int x, T val) where T : struct
         {
             T[] output = new T[x];
-            if (typeof(T).IsValueType)
-                for (int i = 0; i < x; i++)
-                    output[i] = val;
-            else
-                for (int i = 0; i < x; i++)
-                    output[i] = Activator.CreateInstance<T>();
+            for (int i = 0; i < x; i++)
+                output[i] = val;
             return output;
         }
 
@@ -52,28 +52,6 @@ namespace SystemExtensions
                 output[i] = func.Invoke();
             return output;
         }
-
-        /// <summary>
-        /// Returns a new array of length x. Each element is initialized to the return value of the given func delegate, which is given funcArgs as input.
-        /// </summary>
-        /// <typeparam name="T">The array type.</typeparam>
-        /// <typeparam name="TFuncArgs">The type of the input parameter for the func delegate.</typeparam>
-        /// <param name="x">The length of the array.</param>
-        /// <param name="funcArgs">The input given to the func delegate.</param>
-        /// <param name="func">A delegate to initialize each element.</param>
-        /// <returns></returns>
-        public static T[] Create<T, TFuncArgs>(int x, TFuncArgs funcArgs, Func<TFuncArgs, T> func)
-        {
-            T[] output = new T[x];
-            for (int i = 0; i < x; i++)
-                output[i] = func.Invoke(funcArgs);
-            return output;
-        }
-
-
-
-
-
 
 
 
@@ -98,50 +76,25 @@ namespace SystemExtensions
                 output[i] = new T[y];
             return output;
         }
-
-        /// <summary>
-        /// Returns a new jagged 2D array where each inner array's length is specified.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="x"></param>
-        /// <param name="ys"></param>
-        /// <returns></returns>
-        public static T[][] Create<T>(int x, List<int> ys)
-        {
-            if (x != ys.Count) throw new ArgumentException("x was not equal to the Count property of ys.");
-            T[][] output = new T[x][];
-            for (int i = 0; i < x; i++)
-                output[i] = new T[ys[i]];
-            return output;
-        }
         
 
         /// <summary>
-        /// Returns a new 2D array of lengths x, y.  Each element is initialized to either val, if value type, or with new instances from the type's default constructor, if reference type.
+        /// Returns a new 2D array of lengths x, y.  Each element is initialized to val.
         /// </summary>
         /// <typeparam name="T">The array type.</typeparam>
         /// <param name="x">The length of the first array.</param>
         /// <param name="y">The length of each secondary array.</param>
         /// <param name="val">The initialization value.</param>
         /// <returns></returns>
-        public static T[][] Create<T>(int x, int y, T val)
+        public static T[][] Create<T>(int x, int y, T val) where T : struct
         {
             T[][] output = new T[x][];
-
-            if (typeof(T).IsValueType)
-                for (int i = 0; i < x; i++)
-                {
-                    output[i] = new T[y];
-                    for (int j = 0; j < y; j++)
-                        output[i][j] = val;
-                }
-            else
-                for (int i = 0; i < x; i++)
-                {
-                    output[i] = new T[y];
-                    for (int j = 0; j < y; j++)
-                        output[i][j] = Activator.CreateInstance<T>();
-                }
+            for (int i = 0; i < x; i++)
+            {
+                output[i] = new T[y];
+                for (int j = 0; j < y; j++)
+                    output[i][j] = val;
+            }
             return output;
         }
 
@@ -165,27 +118,6 @@ namespace SystemExtensions
             return output;
         }
 
-        /// <summary>
-        /// Returns a new 2D array of lengths x, y. Each element is initialized to the return value of the given func delegate, which is given funcArgs as input.
-        /// </summary>
-        /// <typeparam name="T">The array type.</typeparam>
-        /// <typeparam name="TFuncArgs">The type of the input parameter for the func delegate.</typeparam>
-        /// <param name="x">The length of the first array.</param>
-        /// <param name="y">The length of each secondary array.</param>
-        /// <param name="funcArgs">The input given to the func delegate.</param>
-        /// <param name="func">A delegate to initialize each element.</param>
-        /// <returns></returns>
-        public static T[][] Create<T, TFuncArgs>(int x, int y, TFuncArgs funcArgs, Func<TFuncArgs, T> func)
-        {
-            T[][] output = new T[x][];
-            for (int i = 0; i < x; i++)
-            {
-                output[i] = new T[y];
-                for (int j = 0; j < y; j++)
-                    output[i][j] = func.Invoke(funcArgs);
-            }
-            return output;
-        }
 
 
 
@@ -223,7 +155,7 @@ namespace SystemExtensions
         }
 
         /// <summary>
-        /// Returns a new 3D array of lengths x, y, z.  Each element is initialized to either val, if value type, or with new instances from the type's default constructor, if reference type.
+        /// Returns a new 3D array of lengths x, y, z.  Each element is initialized to val.
         /// </summary>
         /// <typeparam name="T">The array type.</typeparam>
         /// <param name="x">The length of the first array.</param>
@@ -231,31 +163,19 @@ namespace SystemExtensions
         /// <param name="z">The length of each tertiary array.</param>
         /// <param name="val">The initialization value.</param>
         /// <returns></returns>
-        public static T[][][] Create<T>(int x, int y, int z, T val)
+        public static T[][][] Create<T>(int x, int y, int z, T val) where T : struct
         {
             T[][][] output = new T[x][][];
-            if (typeof(T).IsValueType)
-                for (int i = 0; i < x; i++)
+            for (int i = 0; i < x; i++)
+            {
+                output[i] = new T[y][];
+                for (int j = 0; j < y; j++)
                 {
-                    output[i] = new T[y][];
-                    for (int j = 0; j < y; j++)
-                    {
-                        output[i][j] = new T[z];
-                        for (int k = 0; k < z; k++)
-                            output[i][j][k] = val;
-                    }
+                    output[i][j] = new T[z];
+                    for (int k = 0; k < z; k++)
+                        output[i][j][k] = val;
                 }
-            else
-                for (int i = 0; i < x; i++)
-                {
-                    output[i] = new T[y][];
-                    for (int j = 0; j < y; j++)
-                    {
-                        output[i][j] = new T[z];
-                        for (int k = 0; k < z; k++)
-                            output[i][j][k] = Activator.CreateInstance<T>();
-                    }
-                }
+            }
             return output;
         }
 
@@ -284,40 +204,6 @@ namespace SystemExtensions
             }
             return output;
         }
-
-        /// <summary>
-        /// Returns a new 3D array of lengths x, y, z. Each element is initialized to the return value of the given func delegate, which is given funcArgs as input.
-        /// </summary>
-        /// <typeparam name="T">The array type.</typeparam>
-        /// <typeparam name="TFuncArgs">The type of the input parameter for the func delegate.</typeparam>
-        /// <param name="x">The length of the first array.</param>
-        /// <param name="y">The length of each secondary array.</param>
-        /// <param name="z">The length of each tertiary array.</param>
-        /// <param name="funcArgs">The input given to the func delegate.</param>
-        /// <param name="func">A delegate to initialize each element.</param>
-        /// <returns></returns>
-        public static T[][][] Create<T, TFuncArgs>(int x, int y, int z, TFuncArgs funcArgs, Func<TFuncArgs, T> func)
-        {
-            T[][][] output = new T[x][][];
-            for (int i = 0; i < x; i++)
-            {
-                output[i] = new T[y][];
-                for (int j = 0; j < y; j++)
-                {
-                    output[i][j] = new T[z];
-                    for (int k = 0; k < z; k++)
-                        output[i][j][k] = func.Invoke(funcArgs);
-                }
-            }
-            return output;
-        }
-
-
-
-
-
-
-
 
 
 
@@ -357,7 +243,7 @@ namespace SystemExtensions
         }
 
         /// <summary>
-        /// Returns a new 4D array of lengths x, y, z, a.  Each element is initialized to either val, if value type, or with new instances from the type's default constructor, if reference type.
+        /// Returns a new 4D array of lengths x, y, z, a.  Each element is initialized to val.
         /// </summary>
         /// <typeparam name="T">The array type.</typeparam>
         /// <param name="x">The length of the first array.</param>
@@ -366,39 +252,23 @@ namespace SystemExtensions
         /// <param name="a">The length of each 4th array.</param>
         /// <param name="val">The initialization value.</param>
         /// <returns></returns>
-        public static T[][][][] Create<T>(int x, int y, int z, int a, T val)
+        public static T[][][][] Create<T>(int x, int y, int z, int a, T val) where T : struct
         {
             T[][][][] output = new T[x][][][];
-            if (typeof(T).IsValueType)
-                for (int i = 0; i < x; i++)
+            for (int i = 0; i < x; i++)
+            {
+                output[i] = new T[y][][];
+                for (int j = 0; j < y; j++)
                 {
-                    output[i] = new T[y][][];
-                    for (int j = 0; j < y; j++)
+                    output[i][j] = new T[z][];
+                    for (int k = 0; k < z; k++)
                     {
-                        output[i][j] = new T[z][];
-                        for (int k = 0; k < z; k++)
-                        {
-                            output[i][j][k] = new T[a];
-                            for (int m = 0; m < a; m++)
-                                output[i][j][k][m] = val;
-                        }
+                        output[i][j][k] = new T[a];
+                        for (int m = 0; m < a; m++)
+                            output[i][j][k][m] = val;
                     }
                 }
-            else
-                for (int i = 0; i < x; i++)
-                {
-                    output[i] = new T[y][][];
-                    for (int j = 0; j < y; j++)
-                    {
-                        output[i][j] = new T[z][];
-                        for (int k = 0; k < z; k++)
-                        {
-                            output[i][j][k] = new T[a];
-                            for (int m = 0; m < a; m++)
-                                output[i][j][k][m] = Activator.CreateInstance<T>();
-                        }
-                    }
-                }
+            }
             return output;
         }
 
@@ -432,41 +302,6 @@ namespace SystemExtensions
             }
             return output;
         }
-
-        /// <summary>
-        /// Returns a new 4D array of lengths x, y, z, a. Each element is initialized to the return value of the given func delegate, which is given funcArgs as input.
-        /// </summary>
-        /// <typeparam name="T">The array type.</typeparam>
-        /// <typeparam name="TFuncArgs">The type of the input parameter for the func delegate.</typeparam>
-        /// <param name="x">The length of the first array.</param>
-        /// <param name="y">The length of each secondary array.</param>
-        /// <param name="z">The length of each tertiary array.</param>
-        /// <param name="a">The length of each 4th array.</param>
-        /// <param name="funcArgs">The input given to the func delegate.</param>
-        /// <param name="func">A delegate to initialize each element.</param>
-        /// <returns></returns>
-        public static T[][][][] Create<T, TFuncArgs>(int x, int y, int z, int a, TFuncArgs funcArgs, Func<TFuncArgs, T> func)
-        {
-            T[][][][] output = new T[x][][][];
-            for (int i = 0; i < x; i++)
-            {
-                output[i] = new T[y][][];
-                for (int j = 0; j < y; j++)
-                {
-                    output[i][j] = new T[z][];
-                    for (int k = 0; k < z; k++)
-                    {
-                        output[i][j][k] = new T[a];
-                        for (int m = 0; m < a; m++)
-                            output[i][j][k][m] = func.Invoke(funcArgs);
-                    }
-                }
-            }
-            return output;
-        }
-
-
-
 
 
 
@@ -512,7 +347,7 @@ namespace SystemExtensions
         }
 
         /// <summary>
-        /// Returns a new 5D array of lengths x, y, z, a, b.  Each element is initialized to either val, if value type, or with new instances from the type's default constructor, if reference type.
+        /// Returns a new 5D array of lengths x, y, z, a, b.  Each element is initialized to val.
         /// </summary>
         /// <typeparam name="T">The array type.</typeparam>
         /// <param name="x">The length of the first array.</param>
@@ -522,47 +357,27 @@ namespace SystemExtensions
         /// <param name="b">The length of each 5th array.</param>
         /// <param name="val">The initialization value.</param>
         /// <returns></returns>
-        public static T[][][][][] Create<T>(int x, int y, int z, int a, int b, T val)
+        public static T[][][][][] Create<T>(int x, int y, int z, int a, int b, T val) where T : struct
         {
             T[][][][][] output = new T[x][][][][];
-            if (typeof(T).IsValueType)
-                for (int i = 0; i < x; i++)
+            for (int i = 0; i < x; i++)
+            {
+                output[i] = new T[y][][][];
+                for (int j = 0; j < y; j++)
                 {
-                    output[i] = new T[y][][][];
-                    for (int j = 0; j < y; j++)
+                    output[i][j] = new T[z][][];
+                    for (int k = 0; k < z; k++)
                     {
-                        output[i][j] = new T[z][][];
-                        for (int k = 0; k < z; k++)
+                        output[i][j][k] = new T[a][];
+                        for (int m = 0; m < z; m++)
                         {
-                            output[i][j][k] = new T[a][];
-                            for (int m = 0; m < z; m++)
-                            {
-                                output[i][j][k][m] = new T[b];
-                                for (int n = 0; n < b; n++)
-                                    output[i][j][k][m][n] = val;
-                            } 
+                            output[i][j][k][m] = new T[b];
+                            for (int n = 0; n < b; n++)
+                                output[i][j][k][m][n] = val;
                         }
                     }
                 }
-            else
-                for (int i = 0; i < x; i++)
-                {
-                    output[i] = new T[y][][][];
-                    for (int j = 0; j < y; j++)
-                    {
-                        output[i][j] = new T[z][][];
-                        for (int k = 0; k < z; k++)
-                        {
-                            output[i][j][k] = new T[a][];
-                            for (int m = 0; m < a; m++)
-                            {
-                                output[i][j][k][m] = new T[b];
-                                for (int n = 0; n < b; n++)
-                                    output[i][j][k][m][n] = Activator.CreateInstance<T>();
-                            }  
-                        }
-                    }
-                }
+            }
             return output;
         }
 
@@ -601,46 +416,7 @@ namespace SystemExtensions
             }
             return output;
         }
-
-        /// <summary>
-        /// Returns a new 5D array of lengths x, y, z, a, b. Each element is initialized to the return value of the given func delegate, which is given funcArgs as input.
-        /// </summary>
-        /// <typeparam name="T">The array type.</typeparam>
-        /// <typeparam name="TFuncArgs">The type of the input parameter for the func delegate.</typeparam>
-        /// <param name="x">The length of the first array.</param>
-        /// <param name="y">The length of each secondary array.</param>
-        /// <param name="z">The length of each tertiary array.</param>
-        /// <param name="a">The length of each 4th array.</param>
-        /// <param name="b">The length of each 5th array.</param>
-        /// <param name="funcArgs">The input given to the func delegate.</param>
-        /// <param name="func">A delegate to initialize each element.</param>
-        /// <returns></returns>
-        public static T[][][][][] Create<T, TFuncArgs>(int x, int y, int z, int a, int b, TFuncArgs funcArgs, Func<TFuncArgs, T> func)
-        {
-            T[][][][][] output = new T[x][][][][];
-            for (int i = 0; i < x; i++)
-            {
-                output[i] = new T[y][][][];
-                for (int j = 0; j < y; j++)
-                {
-                    output[i][j] = new T[z][][];
-                    for (int k = 0; k < z; k++)
-                    {
-                        output[i][j][k] = new T[a][];
-                        for (int m = 0; m < a; m++)
-                        {
-                            output[i][j][k][m] = new T[b];
-                            for (int n = 0; n < b; n++)
-                                output[i][j][k][m][n] = func.Invoke(funcArgs);
-                        }
-                    }
-                }
-            }
-            return output;
-        }
-
-
-
+        
 
 
 
